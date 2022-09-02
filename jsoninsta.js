6318b738ae052a0ba3d72d254143f0945f7fe0fa
DarkSea1220 <98364095+Darksea1220@users.noreply.github.com>
2022-09-02 00:15:27 -0500

    Adjunto json de insta y tinder en zip (sin modules)

---
+++ jsoninsta.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+        "profileimg":".././images/perfil.png",
+        "name":"Darkseaed1t2",
+        "ubication":"Cali",
+        "post":".././images/perfil.png",
+        "description":`Darkseaed1t2 cambió su foto de perfil!!`
+        
+    },
+    {
+        "profileimg":".././images/chad.jpeg",
+        "name":"Larry Capija",
+        "ubication":"Chupamestepenco",
+        "post":".././images/sisoyXD.png",
+        "description":`No se rían y díganme quién es Rosa Melano`
+    },
+    {
+        "profileimg":".././images/perfil1.jpg",
+        "name":"Rosa Melano",
+        "ubication":"Kissing-Alemania",
+        "post":".././images/jaja.png",
+        "description":`Alguien que le diga quién soy a Larry Capija`
+    },
+    {
+        "profileimg":".././images/ahe.jpeg",
+        "name":"Alan Brito Delgado",
+        "ubication":"Hell-Noruega",
+        "post":".././images/postahe.jpeg",
+        "description":`Siempre caliente, nunca incaliente`
+    },
+    {
+        "profileimg":".././images/perfil2.jpg",
+        "name":"Elza Potes",
+        "ubication":"Australia",
+        "post":".././images/tru.jpg",
+        "description":`AYEDA!!!!!`
+    },
+    {
+        "profileimg":".././images/perfil3.jpg",
+        "name":"Susana Oria",
+        "ubication":"Iran",
+        "post":".././images/ouch.jpeg",
+        "description":`Con permiso que me voy a zuisidar`,
+    },
+    {
+        "profileimg":".././images/perfil5.jpeg",
+        "name":"Elba Lazo",
+        "ubication":"Turquia",
+        "post":".././images/sospechoso.jpg",
+        "description":`Siempre caliente, nunca incaliente`,
+    },
+    {
+        "profileimg":".././images/perfil6.jpeg",
+        "name":"Mario Neta",
+        "ubication":"Conneticut",
+        "post":".././images/posi.jpg",
+        "description":`Siempre caliente, nunca incaliente`,
+    },
+    {
+        "profileimg":".././images/perfil4.jpeg",
+        "name":"Armando Bronca Segura",
+        "ubication":"Suiza",
+        "post":".././images/jsjs.jpg",
+        "description":`No lo sé, solamente pasa`,
+    },
+    {
+        "profileimg":".././images/perfil7.jpg",
+        "name":"Armando esteban Quito",
+        "ubication":"Rusia",
+        "post":".././images/wakuwaku.jpg",
+        "description":`Waku Waku!!!`,
+    },
+]
+export default data;
